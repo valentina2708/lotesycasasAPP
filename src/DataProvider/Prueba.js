8e8c65b8449cff8475d326f61848702d236f759d
Santiago Vivas <sago.vivas@gmail.com>
2023-05-02 22:48:15 -0500

    changes theme, wrapper and urbanizations

--- src/DataProvider/Prueba.js
+++ src/DataProvider/Prueba.js
@@ -1,112 +1,103 @@
+export const Prueba = [
+	{
+		id: '1',
+		adicional: 'v',
+		act: 'v',
+		nombre: 'Rincón Del Lago',
+		subtitulo: ' Condominio Campestre y Plaza Comercial',
+		imagencard: '/src/assets/logoRincondelLago.jpg',
+		mini: '/src/assets/Mini_Rincon_del_lago.jpg',
+		imagenad: '/src/assets/parque-1.jpg',
+		imagenad2: '/src/assets/car2.jpg',
+		tipo: 'Urbanización',
+		descripcion:
+			'A pocos minutos de Cali por las avenidas Panamericana, Cañas Gordas y Sachamate. Cerca' +
+			'de los mejores colegios y universidades del sur' +
+			'occidente colombiano. A pocas cuadras de centros' +
+			'comerciales, súper mercados, restaurantes,' +
+			'parques, gimnasios, entre otros establecimientos' +
+			'que ofrecen servicios de calidad en el ambiente' +
+			'campestre de Jamundí.',
+		descripcionad:
+			'' +
+			'Sin duda una de las zonas más apacibles, seguras y ' +
+			'confortables para vivir en familia o para invertir' +
+			' con confianza gracias al desarrollo urbanístico de' +
+			'este sector por parte de importantes constructoras' +
+			'que cada día mejoran su plusvalía. ' +
+			'Panamericana',
+		lat: 3.2499367481968666,
+		lng: -76.56676798926843,
+		imagen1: '/src/assets/parque2.jpg',
+		imagen2: '/src/assets/parque3.jpg',
+		imagen3: '/src/assets/TERRAZA1.jpg',
+		imagen4: '/src/assets/TERRAZA3.jpg',
+		imagen5: '/src/assets/galeririncon5.jpg',
+		imagen6: '/src/assets/galeririncon6.jpg',
+		imagen7: '/src/assets/galeririncon7.jpg',
+		imagen8: '/src/assets/galeririncon8.jpg',
+		descripcion2:
+			'Proyecto nuevo de la contructora inmobiliaria Casas & Lotes en Jamundí con financiación' +
+			'de hasta 60 meses, 173 lotes de vivienda, 8 lotes comerciales, 64 lotes vendidos con una alta valorización.',
+		ubicacion: 'Jamundí',
+		areaDelLote:
+			'Lotes comerciales desde 108 mts²' +
+			'Lotes de vivienda desde 150 mts²mts²',
+		estado: 'Lanzamiento',
+		imagen: '/src/assets/casasylotes5.jpeg',
+		imgproyectos: 'RinconDelLago',
+		pisina: '/src/assets/pisina1.png',
+		imgzonaSocial: '/src/assets/TERRAZA2.jpg',
+		pagar: '/src/assets/pagar.jpg',
+	},
 
+	{
+		id: 2,
+		nombre: 'Puertas Del Sol',
+		act: 'v',
+		imagencard: '/src/assets/logopuertasdelsol.png',
+		subtitulo: 'Ubicado en Jamundí, a 15 minutos de la ciudad de Cali',
+		estado: 'Finalizado',
+		tipo: 'Proyecto Urbanización',
+		descripcion:
+			'Proyecto de la constructora inmobiliaria Casas & Lotes en Jamundí, 188 lotes para vivienda y 20 lotes comerciales con fachada unificada, Servicios públicos, zardinel, vías pavimentadas, andenes, zonas verdes, parqueaderos, zonas comerciales y estación de servicio.',
+		ubicacion: 'Jamundí, a 15 minutos de la ciudad de Cali',
+		areaDelLote: '120 a 262 mts²- 80 a 140 mts²',
+		imagen: '/src/assets/urbanizacionPuertadelSol.jpg',
+		lat: 3.2499367481968666,
+		lng: -76.56676798926843,
+		imagen1: '/src/assets/galeripuertasdelsol1.png',
+		imagen2: '/src/assets/galeripuertasdelsol2.png',
+		imagen3: '/src/assets/galeripuertasdelsol3.png',
+		imagen4: '/src/assets/galeripuertasdelsol4.png',
+		imagen5: '/src/assets/galeripuertasdelsol5.png',
+		imagen6: '/src/assets/galeripuertasdelsol6.png',
+		imagen7: '/src/assets/galeripuertasdelsol7.JPG',
+		imagen8: '/src/assets/galeripuertasdelsol8.JPG',
+	},
 
-export const Prueba=[
-  
-   
-    {
-        id:'1',
-        adicional:'v',
-        act:'v',
-        nombre: 'Rincón Del Lago',
-        subtitulo:' Condominio Campestre y Plaza Comercial',
-        imagencard:'/src/assets/logoRincondelLago.jpg',
-        mini:'/src/assets/Mini_Rincon_del_lago.jpg',
-        imagenad:'/src/assets/parque-1.jpg',
-        imagenad2:'/src/assets/car2.jpg',
-        tipo: 'Urbanización',
-        descripcion: 'A pocos minutos de Cali por las avenidas Panamericana, Cañas Gordas y Sachamate. Cerca'+
-        'de los mejores colegios y universidades del sur'+
-        'occidente colombiano. A pocas cuadras de centros'+
-        'comerciales, súper mercados, restaurantes,'+
-        'parques, gimnasios, entre otros establecimientos'+
-        'que ofrecen servicios de calidad en el ambiente'+
-        'campestre de Jamundí.',
-        descripcionad:''+
-        'sin duda una de las zonas más apacibles, seguras y'+
-        'confortables para vivir en familia o para invertir'+
-        ' con confianza gracias al desarrollo urbanístico de'+
-        'este sector por parte de importantes constructoras'+
-       'que cada día mejoran su plusvalía.'+
-        'Panamericana',
-        lat:3.2499367481968666,
-        lng:-76.56676798926843,
-        imagen1: '/src/assets/parque2.jpg',
-        imagen2: '/src/assets/parque3.jpg',
-        imagen3: '/src/assets/TERRAZA1.jpg',
-        imagen4: '/src/assets/TERRAZA3.jpg',
-        imagen5: '/src/assets/galeririncon5.jpg',
-        imagen6: '/src/assets/galeririncon6.jpg',
-        imagen7: '/src/assets/galeririncon7.jpg',
-        imagen8: '/src/assets/galeririncon8.jpg',
-        descripcion2: 'Proyecto nuevo de la contructora inmobiliaria Casas & Lotes en Jamundí con financiación'+
-        'de hasta 60 meses, 173 lotes de vivienda, 8 lotes comerciales, 64 lotes vendidos con una alta valorización.',
-        ubicacion:'Jamundí',
-        areaDelLote:'Lotes comerciales desde 108 mts²'+
-        'Lotes de vivienda desde 150 mts²mts²',
-        estado: 'Lanzamiento',
-        imagen: '/src/assets/casasylotes5.jpeg',
-        imgproyectos: 'RinconDelLago',
-        pisina: '/src/assets/pisina1.png', 
-        imgzonaSocial:'/src/assets/TERRAZA2.jpg',
-        pagar:'/src/assets/pagar.jpg'
-       
-       },
+	{
+		id: '3',
+		// adicional: 'n',
+		nombre: 'Palmeras De La Italia',
+		subtitulo: 'Condominio Campestre',
+		imagencard: '/src/assets/logoPalmeras.png',
+		tipo: 'Urbanización',
+		descripcion: 'Condominio campestre de 159 lotes, portería de diseño contemporáneo con lobby de espera; amplia sede social en dos pisos con piscina; circuito para ciclopaseo; cancha deportiva múltiple; 50 parqueaderos de visitantes y zonas verdes. ',
+        ubicacion:'El Paso de la Bolsa',
+		lat: 3.226784,
+		lng: -76.5385101,
+		imagen1: '/src/assets/parque2.jpg',
+		imagen2: '/src/assets/parque3.jpg',
+		imagen3: '/src/assets/TERRAZA1.jpg',
+		imagen4: '/src/assets/TERRAZA3.jpg',
+		descripcion2: 'El proyecto se desarrollará en dos sectores, en un lote de más de 10 hectáreas, tenemos una reserva de más de 3.500 metros cuadrados para futuro desarrollo comercial una vez se encuentren consolidadas las dos primeros sectores de vivienda. En total serán 333 lotes para el desarrollo de vivienda unifamiliar. más de 12.300 metros cuadrados de espacio público, Todo esto enmarcado por el espectacular paisaje del Valle del Cauca, entre frondosos árboles y guaduales.',
+		areaDelLote: '150 m2 - 346 mts²',
+		estado: 'Lanzamiento',
+		imagen: '/src/assets/casasylotes5.jpeg',
+		imgproyectos: 'RinconDelLago',
+		pisina: '/src/assets/pisina1.png',
+		imgzonaSocial: '/src/assets/TERRAZA2.jpg',
 
-       {
-        id:2,
-        nombre: 'Puertas Del Sol',
-        act:'v',
-        imagencard:'/src/assets/logopuertasdelsol.png',
-        subtitulo:'Ubicado en Jamundí, a 15 minutos de la ciudad de Cali',
-        estado: 'Finalizado',
-        tipo: 'Proyecto Urbanización',
-        descripcion: 'Proyecto de la constructora inmobiliaria Casas & Lotes en Jamundí, 188 lotes para vivienda y 20 lotes comerciales con fachada unificada, Servicios públicos, zardinel, vías pavimentadas, andenes, zonas verdes, parqueaderos, zonas comerciales y estación de servicio.',
-        ubicacion: 'Jamundí, a 15 minutos de la ciudad de Cali',
-        areaDelLote: '120 a 262 mts²- 80 a 140 mts²',
-        imagen: '/src/assets/urbanizacionPuertadelSol.jpg',
-        lat:3.2499367481968666,
-        lng:-76.56676798926843,
-        imagen1: '/src/assets/galeripuertasdelsol1.png',
-        imagen2: '/src/assets/galeripuertasdelsol2.png',
-        imagen3: '/src/assets/galeripuertasdelsol3.png',
-        imagen4: '/src/assets/galeripuertasdelsol4.png',
-        imagen5: '/src/assets/galeripuertasdelsol5.png',
-        imagen6: '/src/assets/galeripuertasdelsol6.png',
-        imagen7: '/src/assets/galeripuertasdelsol7.JPG',
-        imagen8: '/src/assets/galeripuertasdelsol8.JPG',
-       },
-
-       {
-        id:'3',
-        adicional:'n',
-        nombre: '000000000000',
-        subtitulo:' 00000000000000',
-        imagencard:'/src/assets/logoRincondelLago.jpg',
-        tipo: 'Urbanización',
-        descripcion: 'A pocos minutos de Cali por las avenidas Panamericana, Cañas Gordas y Sachamate. Cerca'+
-        'de los mejores colegios y universidades del sur'+
-        'occidente colombiano. A pocas cuadras de centros'+
-        'comerciales, súper mercados, restaurantes,'+
-        'parques, gimnasios, entre otros establecimientos'+
-        'que ofrecen servicios de calidad en el ambiente'+
-        'campestre de Jamundí.',
-        lat:3.2499367481968666,
-        lng:-76.56676798926843,
-        imagen1: '/src/assets/parque2.jpg',
-        imagen2: '/src/assets/parque3.jpg',
-        imagen3: '/src/assets/TERRAZA1.jpg',
-        imagen4: '/src/assets/TERRAZA3.jpg',
-        descripcion2: 'Proyecto nuevo de la contructora inmobiliaria Casas & Lotes en Jamundí con financiación de hasta 60 meses, 173 lotes de vivienda, 8 lotes comerciales, 64 lotes vendidos con una alta valorización.',
-        ubicacion:'Jamundí',
-        areaDelLote:'150 m2 - 346 mts²',
-        estado: 'Lanzamiento',
-        imagen: '/src/assets/casasylotes5.jpeg',
-        imgproyectos: 'RinconDelLago',
-        pisina: '/src/assets/pisina1.png', 
-        imgzonaSocial:'/src/assets/TERRAZA2.jpg'
-       
-       },
-
-
-  
-]
+	},
+];
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { CssBaseline } from '@mui/material'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './config/themeConfig';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CssBaseline />
-    <App />
-  </React.StrictMode>
-)
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>
+);
