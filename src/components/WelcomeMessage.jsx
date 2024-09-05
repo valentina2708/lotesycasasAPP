53061362f217e39485279fc65b8ea3c4771ac4b0
valentina2708 <valenpm99082@gmail.com>
2024-09-04 19:16:47 -0500

    Arreglos en home

--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -1,4 +1,3 @@
-
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import { Typography } from '@mui/material';
@@ -11,98 +10,103 @@ import GroupIcon from '@mui/icons-material/Group';
 import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
 import StarIcon from '@mui/icons-material/Star';
 import { cloneElement } from 'react';
+import fondo from '/src/assets/fondo.jpeg'
 
 export default function BasicStack() {
-  const [element, controls] = useScroll();
+	const [element, controls] = useScroll();
 
-  return (
-    <Box
-      ref={element}
-      sx={{
-        width: '100%',
-        minHeight: '50vh', 
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        //backgroundColor: Colors.primary,
-        padding: '5% 0',
-      }}
-    >
-      <motion.div
-        className='milestone'
-        variants={mensajebox}
-        animate={controls}
-        transition={{
-          delay: 0.03,
-          type: 'tween',
-          duration: 0.8,
-        }}
-      >
-   
-        <Box
-          sx={{
-            mt: 1,
-            px: 2,
-          }}
-        >
-          <Grid
-            container
-            spacing={{ xs: 4, md: 6 }}
-            justifyContent='center'
-     alignItems='stretch'
-          >
-            {[
-              { icon: <GroupIcon/>, label: 'Clientes Satisfechos' },
-              { icon: <StarIcon />, label: 'Efectividad' },
-              { icon: <AssignmentTurnedInIcon />, label: 'Proyectos Terminados' },
-            ].map((item, index) => (
-              <Grid
-                item
-                xs={12}
-                sm={6}
-                md={4}
-                key={index}
-                sx={{ display: 'flex', justifyContent: 'center' }}
-              >
-                <Box
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    textAlign: 'center',
-                    gap: 2,
-                    padding: 3,
-                    backgroundColor: Colors.fondo,
-                    borderRadius: 2,
-                    boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1)',
-                    transition: 'transform 0.3s ease',
-                    opacity: [0.9, 0.7, 0.9],
-                    '&:hover': {
-                      transform: 'translateY(-10px)',
-           
-                    
-                    },
-                    minHeight: 180, 
-                    width: '100%', 
-                    maxWidth: 200,
-                  }}
-                >
-                  {cloneElement(item.icon, { sx: { fontSize: 70, color: Colors.info3 } })}
-                  <Typography
-                    variant="h6"
-                    sx={{
-                      color: Colors.body_bg,
-                      fontWeight: '400',
-                    }}
-                  >
-                    {item.label}
-                  </Typography>
-                </Box>
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
-      </motion.div>
-    </Box>
-  );
+	return (
+		<Box
+			ref={element}
+			sx={{
+				width: '100%',
+				minHeight: '40vh',
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				//backgroundColor: Colors.primary,
+        opacity: [0.8, 0.6, 0.8],
+       
+				padding: '5% 0',
+			}}
+		>
+			<motion.div
+				className='milestone'
+				variants={mensajebox}
+				animate={controls}
+				transition={{
+					delay: 0.03,
+					type: 'tween',
+					duration: 0.8,
+				}}
+			>
+				<Box
+					sx={{
+						mt: 1,
+						px: 2,
+					}}
+				>
+					<Grid
+						container
+						spacing={{ xs: 4, md: 6 }}
+						justifyContent='center'
+						alignItems='stretch'
+					>
+						{[
+							{ icon: <GroupIcon />, label: 'Clientes Satisfechos' },
+							{ icon: <StarIcon />, label: 'Efectividad' },
+							{
+								icon: <AssignmentTurnedInIcon />,
+								label: 'Proyectos Terminados',
+							},
+						].map((item, index) => (
+							<Grid
+								item
+								xs={12}
+								sm={6}
+								md={4}
+								key={index}
+								sx={{ display: 'flex', justifyContent: 'center' }}
+							>
+								<Box
+									sx={{
+										display: 'flex',
+										flexDirection: 'column',
+										alignItems: 'center',
+										textAlign: 'center',
+										gap: 2,
+										padding: 3,
+										backgroundColor: Colors.body_bg,
+										borderRadius: 2,
+										boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1)',
+										transition: 'transform 0.3s ease',
+
+										'&:hover': {
+											transform: 'translateY(-10px)',
+										},
+										minHeight: 180,
+										width: '100%',
+										maxWidth: 200,
+									}}
+								>
+									{cloneElement(item.icon, {
+										sx: { fontSize: 70, color: Colors.info3 },
+									})}
+									<Typography
+										variant='h6'
+										sx={{
+											color: Colors.fondo,
+											fontWeight: '500',
+										}}
+									>
+										{item.label}
+									</Typography>
+								</Box>
+							</Grid>
+						))}
+					</Grid>
+				</Box>
+			</motion.div>
+		</Box>
+	);
 }
